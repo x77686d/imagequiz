51fea587569c566a7c6f3c38b992ffd454bcd9ba
William H. Mitchell <whm@msweng.com>
2020-09-29 14:24:15 -0600

    Updates

---
+++ precache-manifest.c5be4b5f7c1299ae4482327a5654e150.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be2b1ba38369578ffdfa7501057408d",
+    "url": "/imagequiz/index.html"
+  },
+  {
+    "revision": "7e890d323dd24ff315bd",
+    "url": "/imagequiz/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "558cd267cd6ed04d71e8",
+    "url": "/imagequiz/static/js/2.dec33d34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/imagequiz/static/js/2.dec33d34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e890d323dd24ff315bd",
+    "url": "/imagequiz/static/js/main.dd873f40.chunk.js"
+  },
+  {
+    "revision": "85da51fa7abe07fb0938",
+    "url": "/imagequiz/static/js/runtime-main.2f3f5ace.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/imagequiz/static/media/logo.5d5d9eef.svg"
+  }
+]);
